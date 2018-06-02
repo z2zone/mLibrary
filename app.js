5ac12c7a90654b2765c169dd58c74b395cea8483
hjun1114 <hjun1114@gmail.com>
2018-06-02 18:55:15 -0400

    setting up passport

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api/index');
 const app = express();
+
+// connect to Mongoose 
+
+mongoose.connect('mongodb://localhost/musiclibrary');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
